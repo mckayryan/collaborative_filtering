bf2537c37a47202002ba4f351b45f6606ac536d0
Ryan <z5060961@zmail.unsw.edu.au>
2017-05-16 18:10:15 +1000

    method for transforming data into appropriate matrix form

---
+++ matrix_formation.py
@@ -0,0 +1,45 @@
+
+# coding: utf-8
+
+# In[6]:
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import numpy as npy
+import pandas as pd
+from numpy import linalg as lla
+
+
+# In[8]:
+
+movies_data = pd.read_csv('/home/ryan/uni/machine_learning/project/data/ml-latest-small/%s.csv' % 'ratings')
+movies_df = pd.DataFrame(movies_data)
+
+
+# In[ ]:
+
+
+
+
+# In[9]:
+
+movies_df.set_index(['userId'])
+movies_df.drop(['timestamp'], axis=1)
+
+
+# In[13]:
+
+movies_df.pivot(index='userId', columns='movieId', values='rating')
+movies_df.as_matrix()
+
+
+# In[ ]:
+
+U,s,V = lla.svd(movies_df, full_matricies=True)
+
+
+# In[ ]:
+
+
+
